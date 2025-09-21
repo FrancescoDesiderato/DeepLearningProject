c05c2e394ef310675f8c9691107c93f1f6418630
Bianca Di Bitetto <bianca.dibitetto@gmail.com>
2025-09-21 10:23:21 +0200

    first attempt to create dataset and BPE

---
+++ bpe.py
@@ -0,0 +1,39 @@
+from tokenizers import Tokenizer
+from tokenizers.models import BPE
+from tokenizers.trainers import BpeTrainer
+from tokenizers.pre_tokenizers import Whitespace
+
+# inizializza un Tokenizer vuoto che userà il modello BPE
+# il `unk_token` è il token che verrà usato se incontra qualcosa di sconosciuto
+tokenizer = Tokenizer(BPE(unk_token="<UNK>"))
+corpus_filename = "corpus.txt"
+
+# imposta un "pre-tokenizer" che divide il testo in parole basandosi sugli spazi
+tokenizer.pre_tokenizer = Whitespace()
+
+# limitazione della dimensione del vocabolario
+VOCAB_SIZE = 32000
+
+# definizione dei token speciali che vogliamo includere nel vocabolario
+# includiamo anche i token standard come PAD (per il padding) e UNK (sconosciuto)
+SPECIAL_TOKENS = [
+    "<UNK>", "<PAD>", "<MASK>",
+    "<SOT>", "<EOT>",
+    "<SUBJ>", "<PRED>", "<OBJ>",
+    "<Text2RDF>", "<RDF2Text>", "<CONTINUERDF>"
+]
+
+# crea un trainer per il tokenizzatore BPE
+trainer = BpeTrainer(vocab_size=VOCAB_SIZE, special_tokens=SPECIAL_TOKENS)
+
+# addestramento tramite il file di corpus
+print(f"Inizio addestramento del tokenizzatore dal file '{corpus_filename}'...")
+tokenizer.train(files=[corpus_filename], trainer=trainer)
+print("Addestramento del tokenizzatore completato.")
+
+# salvataggio del tokenizer
+tokenizer_path = "tokenizer.json"
+tokenizer.save(tokenizer_path)
+
+print(f"Tokenizzatore salvato con successo in '{tokenizer_path}'.")
+
