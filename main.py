f5f7d92fb4ea9909b1f1610a9e25b402222186e2
FrancescoDesiderato <desiderato.francescosalvatore@gmail.com>
2025-10-15 10:26:39 +0200

    Added some Results + minor fixes

--- main.py
+++ main.py
@@ -56,7 +56,7 @@ if __name__ == '__main__':
     tokenizer = PreTrainedTokenizerFast(tokenizer_file=tokenizer_path)
     PAD_IDX = tokenizer.convert_tokens_to_ids("<PAD>")
 
-    device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(device)
 
     if model_training:
